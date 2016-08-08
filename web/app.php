b13d6621fa944919b6dcf3fce85a6ca7116de8b0
Jacob Bare <jacob.bare@cygnus.com>
2016-08-08 14:37:07 -0500

    Update front controller for env

--- web/app.php
+++ web/app.php
@@ -8,12 +8,23 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$env   = strtolower(getenv('APP_ENV'));
+$debug = 'dev' === $env;
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+if ('prod' === $env) {
+    $kernel = new AppKernel('prod', $debug);
+    $kernel->loadClassCache();
+} else {
+    $kernel = new AppKernel('dev', $debug);
+}
+
+// Load AppCache based on server environment variable 'APP_CACHE'
+if (getenv('USE_CACHE') === 'true') {
+    $kernel = new AppCache($kernel);
+    // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+    Request::enableHttpMethodParameterOverride();
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
