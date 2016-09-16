183714f437aa4e1488782c6dbc23d0345202888a
Josh Worden <solocommand@gmail.com>
2016-09-16 13:00:24 -0500

    Moved services to bundle

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -9,6 +9,7 @@ use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,9 +23,23 @@ class AppExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        // Load bundle services
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
+
+        // Load bundle configuration
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        // Additional setup
+        $this->initializeImportServices($container, $config);
+    }
+
+    private function initializeImportServices(ContainerBuilder $container, array $config)
+    {
         foreach ($config['import']['connections'] as $key => $parameters) {
             $name = sprintf('app_bundle.import.connection.%s', $key);
             $def = new Definition();
@@ -32,10 +47,9 @@ class AppExtension extends Extension
             $def->setArguments([$parameters['dsn'], $parameters['options']]);
             $container->setDefinition($name, $def);
 
-            foreach ($container->findTaggedServiceIds(sprintf('import.connection.%s', $key)) as $importer) {
-                $ref = new Reference($name);
-                $importer->setArguments([$ref]);
-            }
+            $ref = new Reference($name);
+            $source = $container->getDefinition(sprintf('app_bundle.import.source.mongo.%s', $key));
+            $source->setArguments([$ref]);
         }
     }
 }
