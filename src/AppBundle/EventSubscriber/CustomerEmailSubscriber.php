78231f248ff381ef1b707de0fd2b9e3d24db7ceb
Jacob Bare <jacob.bare@cygnus.com>
2016-09-21 12:19:49 -0500

    Added new customer account modeling/subscribers

--- src/AppBundle/EventSubscriber/CustomerEmailSubscriber.php
+++ src/AppBundle/EventSubscriber/CustomerEmailSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\EventSubscriber;
 
+use AppBundle\Exception\HttpFriendlyException;
 use AppBundle\Utility\ModelUtility;
 use As3\Modlr\Events\EventSubscriberInterface;
 use As3\Modlr\Models\Model;
@@ -30,9 +31,10 @@ class CustomerEmailSubscriber implements EventSubscriberInterface
             return;
         }
         $this->formatEmailAddress($model);
-        if (null === $model->get('customer')) {
-            throw new \InvalidArgumentException('All customer email addresses must be assigned to a customer.');
+        if (null === $model->get('account')) {
+            throw new HttpFriendlyException('All customer email addresses must be assigned to a account.', 400);
         }
+        $this->handleVerification($model);
     }
 
     /**
@@ -53,12 +55,35 @@ class CustomerEmailSubscriber implements EventSubscriberInterface
         $value = $model->get('value');
         $value = trim($value);
         if (empty($value)) {
-            throw new \InvalidArgumentException('The customer email value cannot be empty.');
+            throw new HttpFriendlyException('The customer email value cannot be empty.', 400);
         }
         $value = strtolower($value);
         if (false === stripos($value, '@')) {
-            throw new \InvalidArgumentException(sprintf('The provided email address "%s" is invalid.', $value));
+            throw new HttpFriendlyException(sprintf('The provided email address "%s" is invalid.', $value), 400);
         }
         $model->set('value', $value);
+    }
+
+    /**
+     * @param   Model   $model
+     */
+    private function handleVerification(Model $model)
+    {
+        if (null === $model->get('verification')) {
+            // Ensure a verification object is always set.
+            $verification = $model->createEmbedFor('verification');
+            $verification->set('verified', false);
+            $model->set('verification', $verification);
+        }
+        if (false === $model->get('verification')->get('verified')) {
+            // Attempting to insert a non-verified email address.
+            // Check if a verified email address already exists. If so, prevent insert.
+            $criteria = ['value' => $model->get('value'), 'verification.verified' => true];
+            $email    = $model->getStore()->findQuery('customer-email', $criteria)->getSingleResult();
+            if (null !== $email) {
+                throw new HttpFriendlyException(sprintf('The customer email address "%s" is already verified and assigned to another account.', $model->get('value')), 400);
+            }
+            $model->get('verification')->set('hash', md5($model->getId()));
+        }
     }
 }
