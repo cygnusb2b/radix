89d3e032b542d6310dc8933cc0a0c0fa417533e6
Jacob Bare <jacob.bare@cygnus.com>
2016-09-26 10:01:39 -0500

    Add payload data parsing tools

--- src/AppBundle/Utility/RequestUtility.php
+++ src/AppBundle/Utility/RequestUtility.php
@@ -29,6 +29,52 @@ class RequestUtility
         if (!isset($payload['data'])) {
             throw new HttpFriendlyException('No data member was found in the request payload.', 422);
         }
-        return (array) $payload['data'];
+        $payload['data'] = (array) $payload['data'];
+        return $payload;
+    }
+
+    /**
+     * Parses flat `target:field.path` keys into an associative array.
+     *
+     * @param   array   $data
+     * @return  array
+     */
+    public static function parsePayloadData(array $data)
+    {
+        $parsed    = [];
+        $parsePath = function($path, $value, &$parsed) {
+            $keys = explode('.', $path);
+            foreach ($keys as $key) {
+                $parsed = &$parsed[$key];
+            }
+            $parsed = $value;
+        };
+
+        foreach ($data as $key => $value) {
+            $value = trim($value);
+            if (empty($value)) {
+                // Skip empty values.
+                continue;
+            }
+            $parts = explode(':', $key);
+            if (2 !== count($parts)) {
+                throw new HttpFriendlyException('The form submission format is invalid. Expected a field keys formatted as [target]:[fieldPath.fieldName].', 422);
+            }
+            $target = $parts[0];
+            $path   = $parts[1];
+
+            if (!isset($parsed[$target])) {
+                $parsed[$target] = [];
+            }
+
+            $parsedValue = null;
+            $parsePath($path, $value, $parsedValue);
+
+            if (!isset($parsed[$target])) {
+                $parsed[$target] = [];
+            }
+            $parsed[$target] = array_merge_recursive($parsed[$target], $parsedValue);
+        }
+        return $parsed;
     }
 }
