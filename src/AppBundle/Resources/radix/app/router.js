cd69ebe1c72968a95e7a9d65853b3d59d6255293
Jacob Bare <jacob.bare@cygnus.com>
2016-10-18 15:43:19 -0500

    Remove uneeded select app route

--- src/AppBundle/Resources/radix/app/router.js
+++ src/AppBundle/Resources/radix/app/router.js
@@ -11,7 +11,6 @@ Router.map(function() {
 
     this.route('user', function() {
         this.route('settings');
-        this.route('select-app');
     });
 
     this.route('modeling', function() {
