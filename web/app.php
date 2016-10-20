31aa8c04cdbef1472fa9d985aaaf6bb0aea8b671
Josh Worden <jworden@southcomm.com>
2016-10-20 11:20:23 -0500

    ip addressing

--- web/app.php
+++ web/app.php
@@ -26,6 +26,14 @@ if (getenv('USE_CACHE') === 'true') {
 }
 
 $request = Request::createFromGlobals();
+
+// Set trusted proxies (varnish) based on server environment variable 'TRUSTED_PROXIES'
+$proxies = [ '127.0.0.1', '192.168.0.1/22' ];
+if (getenv('TRUSTED_PROXIES')) {
+    $proxies = explode(',', getenv('TRUSTED_PROXIES'));
+}
+Request::setTrustedProxies($proxies);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
