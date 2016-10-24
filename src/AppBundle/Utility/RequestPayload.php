86b297fc6cf8cda3d717a5851d649b721661fa69
Jacob Bare <jacob.bare@cygnus.com>
2016-10-24 09:57:06 -0500

    Update request payload to reference identities

--- src/AppBundle/Utility/RequestPayload.php
+++ src/AppBundle/Utility/RequestPayload.php
@@ -15,7 +15,7 @@ class RequestPayload
     /**
      * @var Parameters
      */
-    private $customer;
+    private $identity;
 
     /**
      * @var Parameters
@@ -32,7 +32,7 @@ class RequestPayload
      */
     public function __construct(Request $request = null)
     {
-        $this->customer   = new Parameters();
+        $this->identity   = new Parameters();
         $this->submission = new Parameters();
         $this->meta       = new Parameters();
         $this->extractFrom($request);
@@ -63,8 +63,8 @@ class RequestPayload
         $payload = RequestUtility::extractPayload($request, false);
         $data    = RequestUtility::parsePayloadData($payload['data']);
 
-        if (isset($data['customer'])) {
-            $this->customer->replace($data['customer']);
+        if (isset($data['identity'])) {
+            $this->identity->replace($data['identity']);
         }
         if (isset($data['submission'])) {
             $this->submission->replace($data['submission']);
@@ -82,20 +82,20 @@ class RequestPayload
      */
     public function clear()
     {
-        $this->customer->clear();
+        $this->identity->clear();
         $this->submission->clear();
         $this->meta->clear();
         return $this;
     }
 
     /**
-     * Gets the customer parameters.
+     * Gets the identity parameters.
      *
      * @return  Parameters
      */
-    public function getCustomer()
+    public function getIdentity()
     {
-        return $this->customer;
+        return $this->identity;
     }
 
     /**
@@ -124,7 +124,7 @@ class RequestPayload
     public function toArray()
     {
         return [
-            'customer'      => $this->customer->toArray(),
+            'identity'      => $this->identity->toArray(),
             'submission'    => $this->submission->toArray(),
             'meta'          => $this->meta->toArray(),
         ];
