63c384223b0d6f52c23754c764628e2031f13d6f
Jacob Bare <jacob.bare@cygnus.com>
2016-10-24 10:36:09 -0500

    Rename customer provider to account

--- src/AppBundle/Security/User/AccountProvider.php
+++ src/AppBundle/Security/User/AccountProvider.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-class CustomerProvider implements UserProviderInterface
+class AccountProvider implements UserProviderInterface
 {
     /**
      * @var string|null
