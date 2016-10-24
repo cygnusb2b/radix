0cdf0462a107ba7e66c7f30646858cf116137bd0
Jacob Bare <jacob.bare@cygnus.com>
2016-10-24 10:34:09 -0500

    Rename customer authenticator to account

--- src/AppBundle/Security/Authenticator/AccountAuthenticator.php
+++ src/AppBundle/Security/Authenticator/AccountAuthenticator.php
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\HttpUtils;
 
-class CustomerAuthenticator extends AbstractCoreAuthenticator
+class AccountAuthenticator extends AbstractCoreAuthenticator
 {
     const USERNAME = 'username';
     const PASSWORD = 'password';
