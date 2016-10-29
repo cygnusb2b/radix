df17c29091e60297176b23ae6d7f2ebf60b94c9b
Jacob Bare <jacob.bare@cygnus.com>
2016-10-29 12:01:02 -0500

    Setup initial internal and external routes

---
+++ src/AppBundle/Resources/radix/app/routes/identity/external.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+    loading: Ember.inject.service(),
+
+    limit:  25,
+    offset: 0,
+
+    model: function() {
+        let _this = this;
+        let criteria = {};
+
+        this.get('loading').show();
+
+        return this.store.query('identity-external', {
+            page: {
+                offset: parseInt(this.get('offset')),
+                limit:  parseInt(this.get('limit'))
+            },
+            filter: {
+                query: {
+                    criteria: JSON.stringify(criteria)
+                }
+            },
+            sort: "-createdDate,name",
+        }).then(function(results) {
+            _this.get('loading').hide();
+            return results;
+        }, function() {
+            _this.get('loading').hide();
+        });
+    },
+
+    setupController: function(controller, model) {
+        this._super(controller, model);
+    },
+
+    actions: {
+        recordAdded: function() {
+            this.refresh();
+        }
+    }
+
+});
