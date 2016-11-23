4584cb44f623702d0dbd22f58502da4623f262d4
Jacob Bare <jacob.bare@cygnus.com>
2016-11-23 11:58:03 -0600

    Add primary email computed function

--- src/AppBundle/Resources/radix/app/models/mixins/identity-email-embeddable.js
+++ src/AppBundle/Resources/radix/app/models/mixins/identity-email-embeddable.js
@@ -5,4 +5,17 @@ const { Mixin, computed } = Ember;
 
 export default Mixin.create({
     emails       : fragmentArray('identity-email'),
+    primaryEmail : computed('emails.[]', function() {
+        let primary = null;
+        this.get('emails').forEach(function(email) {
+            if (null === primary) {
+                // Use first email as primary, as a default.
+                primary = email.get('value');
+            }
+            if (email.get('isPrimary')) {
+                primary = email.get('value');
+            }
+        });
+        return primary;
+    }),
 });
