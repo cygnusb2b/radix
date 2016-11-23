cf80b26ded470bed64f01a44aa4f5b1f62f3dc2e
Jacob Bare <jacob.bare@cygnus.com>
2016-11-23 12:13:25 -0600

    Update origins to use model fragments array

--- src/AppBundle/Resources/radix/app/models/core-application.js
+++ src/AppBundle/Resources/radix/app/models/core-application.js
@@ -1,13 +1,13 @@
 import Model                  from 'ember-data/model';
 import attr                   from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
-import { fragment }           from 'model-fragments/attributes';
+import { fragment, array }    from 'model-fragments/attributes';
 import Keyable                from 'radix/models/mixins/keyable';
 import Timestampable          from 'radix/models/mixins/timestampable';
 
 export default Model.extend(Keyable, Timestampable, {
     publicKey      : attr('string'),
-    allowedOrigins : attr('array'),
+    allowedOrigins : array(),
     account        : belongsTo('core-account'),
     users          : hasMany('core-application-user', { inverse: 'user' }),
     settings       : fragment('application-settings'),
