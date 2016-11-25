b8598f4b65c64c36fc7bfcf4047e9b7afeacec52
Jacob Bare <jacob.bare@cygnus.com>
2016-11-25 12:34:19 -0600

    Add sensible defaults to input group

--- src/AppBundle/Resources/radix/app/components/-bootstrap/form/input-group.js
+++ src/AppBundle/Resources/radix/app/components/-bootstrap/form/input-group.js
@@ -1,16 +1,19 @@
 import Ember from 'ember';
 import Group from 'radix/components/-bootstrap/form/group';
 
-const { computed } = Ember;
+const { String: { dasherize }, computed } = Ember;
 
 export default Group.extend({
-    key         : null,
-    value       : null,
-    label       : null,
-    type        : 'text',
-    placeholder : null,
-    helpText    : null,
-    hideLabel   : false,
+    key                 : null,
+    value               : null,
+    label               : null,
+    type                : 'text',
+    placeholder         : null,
+    helpText            : null,
+    hideLabel           : false,
+    hidePlaceholder     : false,
+    calculateLabel      : true,
+    calculatePlaceholder: true,
 
     fieldId     : computed('key', function() {
         let key = this.get('key');
@@ -29,4 +32,25 @@ export default Group.extend({
         }
         return id + '-help';
     }),
+
+    calcLabel : computed('key', 'label', function() {
+        let label = this.get('label');
+        if (!this.get('calculateLabel') || label) {
+            return label;
+        }
+        let parts = dasherize(this.get('key')).split('-');
+        for (var i = 0; i < parts.length; i++) {
+            parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].slice(1);
+        }
+        return parts.join(' ');
+    }),
+
+    calcPlaceholder : computed('key', 'placeholder', function() {
+        let placeholder = this.get('placeholder');
+        if (!this.get('calculatePlaceholder') || placeholder) {
+            return placeholder;
+        }
+        return this.get('calcLabel');
+    }),
+
 });
--- src/AppBundle/Resources/radix/app/routes/product.js
+++ src/AppBundle/Resources/radix/app/routes/product.js
@@ -10,7 +10,7 @@ export default SecureRoute.extend({
                 [
                     { linkTo: 'product.tags', label: 'Tags' },
                 ],
-            ]
+            ];
         }
     }
 });
