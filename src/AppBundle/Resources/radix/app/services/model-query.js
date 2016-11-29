cfa9838c4c190b446c0c73daf182b7d9c9b79851
Jacob Bare <jacob.bare@cygnus.com>
2016-11-29 13:43:43 -0600

    Move query param building to seperate method

--- src/AppBundle/Resources/radix/app/services/model-query.js
+++ src/AppBundle/Resources/radix/app/services/model-query.js
@@ -7,24 +7,39 @@ export default Service.extend({
     store   : service(),
     loading : service(),
 
+    buildParams : function(criteria, limit, offset, sort) {
+        criteria = ('object' === typeOf(criteria)) ? criteria : {};
+
+        if (0 !== limit) {
+            limit = parseInt(limit)  || 25;
+        }
+        offset   = parseInt(offset) || 0;
+        sort     = sort || "-createdDate";
+
+        let params = {
+            filter : { query : { criteria: JSON.stringify(criteria) } },
+            sort   : sort
+        };
+        if (limit > 0) {
+            params.page = {
+                limit  : limit,
+                offset : offset,
+            };
+        }
+        return params;
+    },
+
     execute : function(modelType, criteria, limit, offset, sort) {
         if (!modelType) {
             throw new Error('No model type specified. Unable to perfrom query');
         }
 
-        criteria = ('object' === typeOf(criteria)) ? criteria : {};
-        limit    = parseInt(limit)  || 25;
-        offset   = parseInt(offset) || 0;
-        sort     = sort || "-createdDate";
-
         let loading = this.get('loading');
         loading.show();
 
-        let promise = this.get('store').query(modelType, {
-            page   : { offset : offset, limit : limit },
-            filter : { query : { criteria: JSON.stringify(criteria) } },
-            sort   : sort,
-        });
+        let params  = this.buildParams(criteria, limit, offset, sort);
+        let promise = this.get('store').query(modelType, params);
+
         promise.then((results) => { return results; });
         promise.finally(() => loading.hide());
         return promise;
