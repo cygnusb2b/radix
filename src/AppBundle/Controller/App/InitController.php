7e979bc113e5b098b12a4c39a30c6fc6eba07425
Jacob Bare <jacob.bare@cygnus.com>
2017-01-03 11:22:30 -0600

    Add post settings and ensure defaults are filled

--- src/AppBundle/Controller/App/InitController.php
+++ src/AppBundle/Controller/App/InitController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\App;
 
+use As3\Modlr\Models\Model;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,11 +18,31 @@ class InitController extends AbstractAppController
     {
         $manager    = $this->get('app_bundle.core.account_manager');
         $app        = $manager->getApplication();
+        $this->appendSettings($app);
         $serialized = $this->get('app_bundle.serializer.public_api')->serialize($app);
         $caching    = $this->get('app_bundle.caching.response_cache');
         $response   = new JsonResponse($serialized);
 
         $caching->addStandardHeaders($response, $app->get('updatedDate'), 300);
         return $response;
+    }
+
+    /**
+     * Ensures that the settings are pre-filled with defaults if empty.
+     *
+     * @param   Model   $app
+     */
+    private function appendSettings(Model $app)
+    {
+        $settings = $app->get('settings');
+        if (null === $settings) {
+            $settings = $app->createEmbedFor('settings');
+            $app->set('settings', $settings);
+        }
+        foreach ($settings->getMetadata()->getEmbeds() as $key => $embedMeta) {
+            if (null === $settings->get($key)) {
+                $settings->set($key, $settings->createEmbedFor($key));
+            }
+        }
     }
 }
