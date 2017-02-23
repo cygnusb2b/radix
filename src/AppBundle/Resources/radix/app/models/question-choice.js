c51afc85f3fdcdff1911a11b44aebc868545ac2a
Jacob Bare <jacob@limit0.io>
2017-02-23 15:21:17 -0600

    Display dependent choices within inline editor

--- src/AppBundle/Resources/radix/app/models/question-choice.js
+++ src/AppBundle/Resources/radix/app/models/question-choice.js
@@ -1,3 +1,4 @@
+import Ember                   from 'ember';
 import Model                   from 'ember-data/model';
 import attr                    from 'ember-data/attr';
 import { belongsTo, hasMany }  from 'ember-data/relationships';
@@ -5,6 +6,8 @@ import Integrateable           from 'radix/models/mixins/integrateable';
 import Sequenceable            from 'radix/models/mixins/sequenceable';
 import SoftDeleteable          from 'radix/models/mixins/soft-deleteable';
 import Timestampable           from 'radix/models/mixins/timestampable';
+
+const { computed } = Ember;
 
 export default Model.extend(Integrateable, Sequenceable, SoftDeleteable, Timestampable, {
     alternateId : attr('string'),
@@ -14,4 +17,8 @@ export default Model.extend(Integrateable, Sequenceable, SoftDeleteable, Timesta
     name        : attr('string'),
     question    : belongsTo('question'),
     dependents  : hasMany('question-choice', { inverse: null }),
+
+    hasDependents : computed('dependents.[]', function() {
+      return this.get('dependents.length') > 0;
+    }),
 });
