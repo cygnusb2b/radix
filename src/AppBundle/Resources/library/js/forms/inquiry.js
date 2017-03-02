61f0e9b5578a3acdc7794d96ac7d35b7c38540e8
Jacob Bare <jacob@limit0.io>
2017-03-01 22:29:06 -0600

    Update inquiry form to new format
    Note: the questions are currently hardcoded to FCP and, as such, will break on other apps. Need to setup form definitions so this can load dynamically and use one form component.

--- src/AppBundle/Resources/library/js/forms/inquiry.js
+++ src/AppBundle/Resources/library/js/forms/inquiry.js
@@ -1,49 +1,63 @@
 React.createClass({ displayName: 'FormInquiry',
-
-    // @todo:: Should these forms take the account object directly, or simply the form field values??
-    getDefaultProps: function() {
-        return {
-            account  : {},
-            onSubmit : function(event) { Debugger.error('Nothing handled the form submit.');     },
-            fieldRef : function(input) { Debugger.error('Nothing handled the field reference.'); }
-        }
-    },
-
-    render: function() {
-        return (this._getForm())
-    },
-
-    _getForm: function() {
-        var account      = this.props.account;
-        var disableEmail = (account._id) ? true : false;
-        var phoneType    = account.primaryPhone.phoneType || 'Phone';
-        var phoneLabel   = phoneType + ' #';
-
-        return React.createElement('form', { autocomplete: false, className: 'database-form', onSubmit: this.props.onSubmit },
-            React.createElement(Radix.Components.get('FormInputHidden'), { ref: this.props.fieldRef, name: 'identity:primaryAddress.identifier', value: account.primaryAddress.identifier }),
-            React.createElement(Radix.Components.get('FormInputHidden'), { ref: this.props.fieldRef, name: 'identity:primaryPhone.identifier', value: account.primaryPhone.identifier }),
-            React.createElement('div', null,
-                React.createElement(Radix.Components.get('FormInputText'), { ref: this.props.fieldRef, name: 'identity:givenName', wrapperClass: 'givenName', label: 'First Name', required: true, value: account.givenName }),
-                React.createElement(Radix.Components.get('FormInputText'), { ref: this.props.fieldRef, name: 'identity:familyName', wrapperClass: 'familyName', label: 'Last Name', required: true, value: account.familyName })
-            ),
-            React.createElement('div', null,
-                React.createElement(Radix.Components.get('FormInputText'), { ref: this.props.fieldRef, type: 'email', name: 'identity:primaryEmail', wrapperClass: 'email', label: 'Email Address', required: !disableEmail, readonly: disableEmail, value: account.primaryEmail }),
-                React.createElement(Radix.Components.get('FormInputText'), { ref: this.props.fieldRef, type: 'tel', name: 'identity:primaryPhone.number', wrapperClass: 'phone', label: phoneLabel, value: account.primaryPhone.number })
-            ),
-            React.createElement('div', null,
-                React.createElement(Radix.Components.get('FormInputText'), { ref: this.props.fieldRef, name: 'identity:companyName', wrapperClass: 'companyName', label: 'Company Name', value: account.companyName }),
-                React.createElement(Radix.Components.get('FormInputText'), { ref: this.props.fieldRef, name: 'identity:title', wrapperClass: 'title', label: 'Job Title', required: true, value: account.title })
-            ),
-            React.createElement('div', null,
-                React.createElement(Radix.Components.get('CountryPostalCode'), { fieldRef: this.props.fieldRef, postalCode: account.primaryAddress.postalCode, countryCode: account.primaryAddress.countryCode, required: true })
-            ),
-            React.createElement('div', null,
-                React.createElement(Radix.Components.get('FormQuestion'), { fieldRef: this.props.fieldRef, keyOrId: 'purchase-intent', answers: account.answers }),
-                React.createElement(Radix.Components.get('FormQuestion'), { fieldRef: this.props.fieldRef, tagKeyOrId: 'business-code', required: true, answers: account.answers }),
-                React.createElement(Radix.Components.get('FormQuestion'), { fieldRef: this.props.fieldRef, tagKeyOrId: 'title-code', required: true, answers: account.answers }),
-                React.createElement(Radix.Components.get('FormQuestion'), { fieldRef: this.props.fieldRef, keyOrId: 'comments', answers: account.answers })
-            ),
-            React.createElement('button', { type: 'submit'}, 'Submit')
-        );
+  // @todo:: Should these forms take the account object directly, or simply the form field values??
+  getDefaultProps: function() {
+    return {
+      account  : {},
+      values   : {},
+      onSubmit : null
     }
+  },
+
+  getInitialState: function() {
+    // @todo State should probably be handled by the parent component.
+    return this.props.values;
+  },
+
+  submitForm: function(event) {
+    event.preventDefault();
+    if ('function' === typeof this.props.onSubmit) {
+      this.props.onSubmit(this.state);
+    } else {
+      throw new Error('Nothing handled the form submit!');
+    }
+  },
+
+  updateFieldValue: function(event) {
+    var stateSlice = {};
+    stateSlice[event.target.name] = event.target.value;
+    this.setState(stateSlice);
+  },
+
+  render: function() {
+    return (this._getForm())
+  },
+
+  _findAnswerFor: function(key) {
+    return this.state[key];
+  },
+
+  _getForm: function() {
+    // @todo This should likely be handled by the form definitions.
+    var account      = this.props.account;
+    var disableEmail = (account._id) ? true : false;
+    var phoneType    = account.primaryPhone.phoneType || 'Phone';
+    var phoneLabel   = phoneType + ' #';
+
+    return React.createElement('form', { autocomplete: false, className: 'database-form', onSubmit: this.submitForm },
+      React.createElement(Radix.Components.get('FormInputHidden'), { name: 'identity:primaryAddress.identifier', value: this.state['identity:primaryAddress.identifier'] }),
+      React.createElement(Radix.Components.get('FormInputHidden'), { name: 'identity:primaryPhone.identifier', value: this.state['identity:primaryPhone.identifier'] }),
+      React.createElement(Radix.Components.get('FormInputText'), { onChange: this.updateFieldValue, name: 'identity:givenName', wrapperClass: 'givenName', label: 'First Name', required: true, value: this.state['identity:givenName'] }),
+      React.createElement(Radix.Components.get('FormInputText'), { onChange: this.updateFieldValue, name: 'identity:familyName', wrapperClass: 'familyName', label: 'Last Name', required: true, value: this.state['identity:familyName'] }),
+      React.createElement(Radix.Components.get('FormInputText'), { onChange: this.updateFieldValue, type: 'email', name: 'identity:primaryEmail', wrapperClass: 'email', label: 'Email Address', required: !disableEmail, readonly: disableEmail, value: this.state['identity:primaryEmail'] }),
+      React.createElement(Radix.Components.get('FormInputText'), { onChange: this.updateFieldValue, type: 'tel', name: 'identity:primaryPhone.number', wrapperClass: 'phone', label: phoneLabel, value: this.state['identity:primaryPhone.number'] }),
+      React.createElement(Radix.Components.get('FormInputText'), { onChange: this.updateFieldValue, name: 'identity:companyName', wrapperClass: 'companyName', label: 'Company Name', value: this.state['identity:companyName'] }),
+      React.createElement(Radix.Components.get('FormInputText'), { onChange: this.updateFieldValue, name: 'identity:title', wrapperClass: 'title', label: 'Job Title', required: true, value: this.state['identity:title'] }),
+      React.createElement(Radix.Components.get('CountryPostalCode'), { onChange: this.updateFieldValue, postalCode: { name: 'identity:primaryAddress.postalCode', value: this.state['identity:primaryAddress.postalCode'] }, countryCode: { name: 'identity:primaryAddress.countryCode', value: this.state['identity:primaryAddress.countryCode'] }, required: false }),
+      React.createElement(Radix.Components.get('FormQuestion'), { onLookup: this._findAnswerFor, onChange: this.updateFieldValue, questionId: '580f6cff39ab465c2caf74ad', value: this.state['submission:answers.580f6cff39ab465c2caf74ad'] }),
+      React.createElement(Radix.Components.get('FormQuestion'), { onLookup: this._findAnswerFor, onChange: this.updateFieldValue, questionId: '583c410839ab46dd31cbdf6d', value: this.state['identity:answers.583c410839ab46dd31cbdf6d'], required: false }),
+      React.createElement(Radix.Components.get('FormQuestion'), { onLookup: this._findAnswerFor, onChange: this.updateFieldValue, questionId: '580f6b3bd78c6a78830041bb', value: this.state['identity:answers.580f6b3bd78c6a78830041bb'], required: true }),
+      React.createElement(Radix.Components.get('FormQuestion'), { onLookup: this._findAnswerFor, onChange: this.updateFieldValue, questionId: '580f6d056cdeea4730ddbb2c', value: this.state['submission:answers.580f6d056cdeea4730ddbb2c'] }),
+      React.createElement('button', { type: 'submit'}, 'Submit')
+    );
+  }
 });
