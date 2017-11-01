30cb1f8353e3ad116aedc4cf8f6e2c85aba1b891
Brian Miller <mills_77_77@hotmail.com>
2017-11-01 11:07:29 -0500

    Add Role Support to Radix Management

--- src/AppBundle/Resources/radix/app/models/core-user.js
+++ src/AppBundle/Resources/radix/app/models/core-user.js
@@ -2,8 +2,10 @@ import Model         from 'ember-data/model';
 import attr          from 'ember-data/attr';
 import { hasMany }   from 'ember-data/relationships';
 import Timestampable from 'radix/models/mixins/timestampable';
+import { array } from 'ember-data-model-fragments/attributes';
 
 export default Model.extend(Timestampable, {
+    roles      : array('string', { defaultValue: ['ROLE_CORE\\USER'] }),
     givenName  : attr('string'),
     familyName : attr('string'),
     email      : attr('string'),
