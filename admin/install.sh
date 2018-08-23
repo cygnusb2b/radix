4a85c093e04b6c73aae55f0bf4d1392f5a071e39
Josh Worden <solocommand@gmail.com>
2018-08-23 10:52:46 -0500

    Moved radix app to /admin

--- admin/install.sh
+++ admin/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-npm install --global yarn
+npm install --global yarn bower
 # npm install -g npm@5.3.0
 # npm cache clean
 # npm install
 yarn install
 bower install --allow-root
-ember build --watch
