a15b2cf9349fd2d110e879b64829ac5216c6ee8b
Josh Worden <josh@limit0.io>
2020-07-16 17:37:48 -0500

    Update .travis.yml

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,16 +2,9 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-mv services ../
-mkdir services
-mv ../services/$1 services/
+cd services/$1
 
-mv "services/$1/Dockerfile" Dockerfile
 docker build -t "radix-$1:$2" --build-arg SERVICE=$1 .
-
-mv ../services/* services/
-rm -rf ../services
-
 docker tag "radix-$1:$2" "endeavorb2b/radix-$1:$2"
 docker push "endeavorb2b/radix-$1:$2"
 docker image rm "radix-$1:$2"
