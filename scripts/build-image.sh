8295279ccff3c264175d674106833d9a9ef67e0f
Josh Worden <josh@limit0.io>
2020-07-17 15:51:07 -0500

    Add support for customizing the docker hub org

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,9 +2,11 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+DOCKER_ORG="${DOCKER_ORG-endeavorb2b}"
+
 cd $1
 
 docker build -t "radix-$1:$2" --build-arg SERVICE=$1 .
-docker tag "radix-$1:$2" "endeavorb2b/radix-$1:$2"
-docker push "endeavorb2b/radix-$1:$2"
+docker tag "radix-$1:$2" "$DOCKER_ORG/radix-$1:$2"
+docker push "$DOCKER_ORG/radix-$1:$2"
 docker image rm "radix-$1:$2"
