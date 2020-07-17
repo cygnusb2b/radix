9187e046f0aacbd1e80ec24fa5c160fc2fc0c2fe
Josh Worden <josh@limit0.io>
2020-07-16 19:23:46 -0500

    Change default port and allow complex mongo uris

--- services/graph/src/env.js
+++ services/graph/src/env.js
@@ -1,17 +1,9 @@
-const { isURL } = require('validator');
-
 const {
   cleanEnv,
   makeValidator,
   port,
   bool,
 } = require('envalid');
-
-const mongodsn = makeValidator((v) => {
-  const opts = { protocols: ['mongodb'], require_tld: false, require_protocol: true };
-  if (isURL(v, opts)) return v;
-  throw new Error('Expected a Mongo DSN string with mongodb://');
-});
 
 const nonemptystr = makeValidator((v) => {
   const err = new Error('Expected a non-empty string');
@@ -26,10 +18,9 @@ const nonemptystr = makeValidator((v) => {
 module.exports = cleanEnv(process.env, {
   APP_HOST: nonemptystr({ desc: 'The hostname where the server instance is running.' }),
   MONGOOSE_DEBUG: bool({ desc: 'Whether to enable Mongoose debugging.', default: false }),
-  MONGO_DSN: mongodsn({ desc: 'The MongoDB DSN to connect to.' }),
-  PORT: port({ desc: 'The port that express will run on.', default: 8100 }),
+  MONGO_DSN: nonemptystr({ desc: 'The MongoDB DSN to connect to.' }),
+  PORT: port({ desc: 'The port that express will run on.', default: 80 }),
   SENDGRID_API_KEY: nonemptystr({ desc: 'The Sendgrid email API key' }),
   SENDGRID_FROM: nonemptystr({ desc: 'The From: address for Sendgrid emails' }),
   JWT_SECRET: nonemptystr({ desc: 'The secret key used to encode Json Web Tokens' }),
 });
-
